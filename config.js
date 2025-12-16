d551e847bdb6bbe019c41908cbaef769100d8791
Shen-M0 <shin940129@gmail.com>
2025-12-16 17:11:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,14 @@
-// 將此處替換為你剛剛複製的 API Gateway Invoke URL
 const CONFIG = {
-    API_URL: "https://pcczjpzgn1.execute-api.us-east-1.amazonaws.com/prod"
+    // 請確認這是你最新的 API Gateway Invoke URL
+    API_URL: "https://pcczjpzgn1.execute-api.us-east-1.amazonaws.com/prod",
+    
+    // 集中管理的書籍分類
+    CATEGORIES: [
+        "電腦科學",
+        "文學小說",
+        "商業理財",
+        "人文歷史",
+        "語言學習",
+        "其他"
+    ]
 };
